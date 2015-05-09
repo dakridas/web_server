8205ce7e1d6f95992341cba43e4804a645fcc17a
aristotelis <aris7kol@yahoo.gr>
2015-05-09 16:01:30 +0300

    write in log files i Please enter the commit message for your changes. Lines starting

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,5 +1,7 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
+import java.text.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
@@ -13,9 +15,14 @@ public class ServerThread implements Runnable {
     private ServerResponse response;
     private String inputLine;
     private ServerIO io;
+    private SimpleDateFormat ft;
+    private Date dnow;
+    private String logstr;
+    private Log logWrite;
 
     public ServerThread(Socket socket,String accessPath,String errorPath) {
         this.socket = socket;
+        logWrite = new Log(accessPath, errorPath);
     }
 
     public void run() {
@@ -24,23 +31,47 @@ public class ServerThread implements Runnable {
             System.out.printf("Connected to ");
             System.out.printf(socket.getInetAddress().toString());
             System.out.printf(":"+socket.getPort()+"\n");
+
+            logstr = socket.getInetAddress().toString() + " - [";
+            ft = new SimpleDateFormat("E MMMM dd hh:mm:ss zzz yyyy");
+            Date dnow = new Date();
+            logstr = logstr + ft.format(dnow) + "] ";
+
             // read the input
             inputLine = io.readLine();
             if (inputLine.startsWith("GET")) {
                 String parametres [] = inputLine.split("\\s");
                 pathFile = parametres[1];
                 httpVersion = parametres[2];
-                response = new ServerResponse(pathFile,httpVersion,io);
-                // wait for header connection and close
-                while ((inputLine = io.readLine()) != null) {
-                    if (inputLine.startsWith("Connection")) {
-                        response.sendResponse();
-                        socket.close();
-                    }
+
+                if (!(httpVersion.equals("HTTP/1.0")) && !(httpVersion.equals("HTTP/1.1"))) {
+                    logstr = logstr + inputLine + " 400 Bad Request";
+                    logWrite.writeToError(logstr);
+                    logWrite.closeFiles();
                 }
+                else {
+                    logstr = logstr + inputLine + " -> 200 OK ";
+                    response = new ServerResponse(pathFile,httpVersion,io);
+                    // wait for header connection and close
+                    while ((inputLine = io.readLine()) != null) {
+                        if (inputLine.startsWith("User-Agent")) {
+                            logstr = logstr + "\"" + inputLine + "\"";
+                        }
+                        if (inputLine.startsWith("Connection")) {
+                            response.sendResponse();
+                            break;
+                        }
+                    }
+                    logWrite.writeToAccess(logstr);
+                    logWrite.closeFiles();
+                }
+                socket.close();
             // 405 fail
             }else {
-                io.writeString("405 Method Not Allowed");
+                logstr = logstr + inputLine + " 405 Method Not Allowed";
+                logWrite.writeToError(logstr);
+                logWrite.closeFiles();
+                socket.close();
             }
             // close io
             io.closeIO();
