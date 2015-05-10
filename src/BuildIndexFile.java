b19ede50b61976010e18cbc5a568fa027f4abcc8
Dimitris Akridas <d.akridas@gmail.com>
2015-05-10 10:32:15 +0300

    complete build index.html

--- src/BuildIndexFile.java
+++ src/BuildIndexFile.java
@@ -50,38 +50,50 @@ public class BuildIndexFile {
     private void writeBody(String path) {
         try{
             String str;
-            str = "    <body>\n";
+            str = "  <body>\n";
             fileOut.write(str.getBytes());
             fileOut.flush();
-            str = "      <article>\n";
+            str = "    <article>\n";
             fileOut.write(str.getBytes());
             fileOut.flush();
-            str = "        <ol>\n";
+            str = "      <ol>\n";
             fileOut.write(str.getBytes());
             fileOut.flush();
             // check path for root directory
             String [] results = path.split("\\/");
             if (results.length > 1) {
-                str = String.format("          <li class=\"directory\"><a href=\"%s/\">%s</a></li>\n","..","parent directory");
+                str = String.format("        <li class=\"directory\"><a href=\"%s/\">%s</a></li>\n","..","parent directory");
             }
             fileOut.write(str.getBytes());
             fileOut.flush();
             if (files != null) {
                 for (File curr : files) {
                     if (curr.isDirectory()) {
-                        str = String.format("          <li class=\"directory\"><a href=\"%s/\">%s</a></li>\n",curr.getName(),curr.getName());
+                        str = String.format("        <li class=\"directory\"><a href=\"%s/\">%s</a></li>\n",curr.getName(),curr.getName());
                         fileOut.write(str.getBytes());
                         fileOut.flush();
                     }
                 }
                 for (File curr : files) {
                     if (!curr.isDirectory() && (!(curr.getName()).equals("index.html"))) {
-                        str = String.format("          <li class=\"file\"><a href=\"%s\">%s</a></li>\n",curr.getName(),curr.getName());
+                        str = String.format("        <li class=\"file\"><a href=\"%s\">%s</a></li>\n",curr.getName(),curr.getName());
                         fileOut.write(str.getBytes());
                         fileOut.flush();
                     }
                 }
             }
+            str = "      </ol>\n";
+            fileOut.write(str.getBytes());
+            fileOut.flush();
+            str = "    </article>\n";
+            fileOut.write(str.getBytes());
+            fileOut.flush();
+            str = "  </body>\n";
+            fileOut.write(str.getBytes());
+            fileOut.flush();
+            str = "</html>\n";
+            fileOut.write(str.getBytes());
+            fileOut.flush();
 
             fileOut.close();
 
